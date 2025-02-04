48b608038028a5ef950d992e7c8beed492b60b2e
Keks <keks@htmlacademy.ru>
2025-02-04 20:05:38 +0300

    :heavy_check_mark: Сборка #3

--- 3/js/main.js
+++ 3/js/main.js
@@ -1,4 +1,53 @@
-const description = [
+//Функция получения уникального идентификатора
+const createIdGenerator = (start = 0) => {
+  let lastGeneratedId = start;
+  return function() {
+    lastGeneratedId += 1;
+    return lastGeneratedId;
+  };
+};
+  //Функция получения уникального идентификатора (начинает сначала при достижении заданного значения)
+const createCircleGenerator = (maxCount) => {
+  let lastGeneratedId = 0;
+  return function() {
+    if(lastGeneratedId >= maxCount) {
+      lastGeneratedId = 0;
+      return lastGeneratedId++;
+    }
+    return lastGeneratedId++;
+  };
+};
+  //Функция получения случайного значения в заданных пределах
+const getRandomInteger = (min, max) => {
+  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+  //Функция получения случайного элемента массива
+const getRandomArrayElement = (elements) => (
+  elements[getRandomInteger(0, elements.length - 1)]
+);
+
+const COUNT_ITEMS = 25;
+const avatarRange = {
+  MIN: 1,
+  MAX: 6
+};
+const likesRange = {
+  MIN: 15,
+  MAX: 200
+};
+const commentsRange = {
+  MIN: 3,
+  MAX: 15
+};
+const commentMessageRange = {
+  MIN: 1,
+  MAX: 2
+};
+const MIN_ID_COMMENT = 100;
+const DESCRIPTIONS = [
   'Озеро',
   'Табличка',
   'Море',
@@ -25,7 +74,7 @@ const description = [
   'Концерт',
   'Сафари',
 ];
-const message = [
+const MESSAGES = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
@@ -33,7 +82,7 @@ const message = [
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
 ];
-const nameСommentator = [
+const NAMES = [
   'Артём',
   'Вася',
   'Петя',
@@ -45,29 +94,24 @@ const nameСommentator = [
   'Евгения',
   'Екатерина',
 ];
-const id = Array.from({length: 25}, (_, i) => i + 1);
-const getRandomInteger = (a, b) => {
-  const lower = Math.ceil(Math.min(a, b));
-  const upper = Math.floor(Math.max(a, b));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
-};
-
-const randomId = getRandomInteger(0, id.length - 1);
-const comment = () => ({
-  id: getRandomInteger(1, 9999),
-  avatar: `img/avatar-${ getRandomInteger(1, 6) }.svg`,
-  message: message[getRandomInteger(0, message.length - 1)],
-  name: nameСommentator[getRandomInteger(0, nameСommentator.length - 1)],
+const generateItemId = createIdGenerator();
+const generateItemPathId = createIdGenerator();
+const generateDescriptionId = createCircleGenerator(DESCRIPTIONS.length);
+const generateCommentId = createIdGenerator(MIN_ID_COMMENT);
+const getCommentMessage = () => Array.from({length: getRandomInteger(commentMessageRange.MIN, commentMessageRange.MAX)},
+  () => getRandomArrayElement(MESSAGES)).join(' ');
+const createComment = () => ({
+  id: generateCommentId(),
+  avatar: `img/avatar-${getRandomInteger(avatarRange.MIN, avatarRange.MAX)}.svg`,
+  message: getCommentMessage(),
+  name: getRandomArrayElement(NAMES)
 });
-
-const comments = Array.from({length: 31}, comment);
-const commentsCount = getRandomInteger(0, comments.length - 1);
-
-const photo = {
-  id: randomId,
-  url: `photos/${ randomId }.jpg`,
-  description: description[randomId - 1],
-  likes: getRandomInteger(15, 200),
-  comments: Array.from({length: commentsCount}, comment),
-};
+const createItem = () => ({
+  id: generateItemId(),
+  url: `photos/${generateItemPathId()}.jpg`,
+  description: DESCRIPTIONS[generateDescriptionId()],
+  likes: getRandomInteger(likesRange.MIN, likesRange.MAX),
+  comments: Array.from({length: getRandomInteger(commentsRange.MIN, commentsRange.MAX)}, createComment)
+});
+const generatePosts = () => Array.from({length: COUNT_ITEMS}, createItem);
+console.log(generatePosts());
